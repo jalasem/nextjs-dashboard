4d0e3cd8b59b6c8bcca935d17292d79112498ea2
AbdulSamii Ajala <kgasta@gmail.com>
2023-11-12 01:29:31 +0100

    chapter 9: streaming

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "@/app/ui/global.css";
 
 import { inter } from "@/app/ui/fonts";
 
+// export const revalidate = 3600 // revalidate at most every hour
+
 export default function RootLayout({
   children,
 }: {
